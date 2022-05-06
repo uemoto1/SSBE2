e41bed34821cee7ca6e746d6b8ad6821ee27593c
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-05-06 13:19:01 +0900

    hotfix

--- time_evolution.f90
+++ time_evolution.f90
@@ -32,9 +32,9 @@ subroutine dt_evolve_bloch(rt, gs, dt, Ac0, Ac1)
     type(rt_data), intent(inout) :: rt
     type(gs_data), intent(in) :: gs
     real(8), intent(in) :: dt, Ac0(3), Ac1(3)
-!     complex(8) :: drho1(rt%nstate, rt%nstate, rt%nk)
-!     complex(8) :: rho2(rt%nstate, rt%nstate, rt%nk)
-!     complex(8) :: drho2(rt%nstate, rt%nstate, rt%nk)
+    complex(8) :: drho1(rt%nstate, rt%nstate, rt%nk)
+    complex(8) :: rho2(rt%nstate, rt%nstate, rt%nk)
+    complex(8) :: drho2(rt%nstate, rt%nstate, rt%nk)
 
      ! Modified Euler
      write(*,*) "START"; flush(6)
