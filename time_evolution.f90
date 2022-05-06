5e96b582c91ed42bbdb7afd842c9dba6aa503ec0
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-05-06 13:36:45 +0900

    hotfix

--- time_evolution.f90
+++ time_evolution.f90
@@ -66,25 +66,25 @@ subroutine calc_drho(drho, rho, Ac)
     drho(:, :, :) = 0.0d0
     write(*,*) "calc_drho1"; flush(6)
     ! stop "HEYHEY"
-    ! !!$omp parallel do default(shared) private(ik,n,i,j,l) collapse(3)
+    !!$omp parallel do default(shared) private(ik,n,i,j,l) collapse(3)
     do ik = 1, rt%nk
         write(*,*) 3; flush(6)
-    !     do i = 1, rt%nstate
+        do i = 1, rt%nstate
     !         write(*,*) 4; flush(0)
-    !         do j = 1, rt%nstate
+            do j = 1, rt%nstate
     !             write(*,*) 5; flush(0)
-    !             do n = 1, 3
+                do n = 1, 3
     !                 write(*,*) 6; flush(0)
-    !                 do l = 1, rt%nstate
-    !                     write(*,*) 7; flush(0)
+                    do l = 1, rt%nstate
+                        write(*,*) 7; flush(0)
     !                     drho(i, j, ik) = drho(i, j, ik) + dcmplx(0.0, -1.0) * Ac(n) &
     !                     & * ((gs%pmatrix(i, l, n, ik) + gs%rvnl(i, l, n, ik)) * rho(l, j, ik) &
     !                     & -  rho(i, l, ik) * (gs%pmatrix(l, j, n, ik) + gs%rvnl(l, j, n, ik)))
     !                     write(*,*) 8; flush(0)
-    !                 end do
-    !             end do
-    !         end do
-    !     end do
+                    end do
+                end do
+            end do
+        end do
     end do
     ! !!$omp end parallel do
 end subroutine calc_drho
