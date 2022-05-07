d63cbb48ec35a9a79cd11bb974af8fe04f0256a8
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-05-07 16:31:42 +0900

    Support v.2.1.0 files

--- sbe_gs.f90
+++ sbe_gs.f90
@@ -18,6 +18,7 @@ module sbe_gs
         real(8), allocatable :: delta_omega(:, :, :)
         complex(8), allocatable :: p_matrix(:, :, :, :)
         complex(8), allocatable :: d_matrix(:, :, :, :)
+        complex(8), allocatable :: tm_p_matrix(:, :, :, :)
         complex(8), allocatable :: rvnl_matrix(:, :, :, :)
         ! complex(8), allocatable :: prod_dk(:, :, :, :, :, :)
 
@@ -60,6 +61,7 @@ subroutine init_sbe_gs(gs, sysname, gs_directory, nkgrid, nb, ne, a1, a2, a3, re
     allocate(gs%delta_omega(1:nb, 1:nb, 1:nk))
     allocate(gs%p_matrix(1:nb, 1:nb, 1:3, 1:nk))
     allocate(gs%d_matrix(1:nb, 1:nb, 1:3, 1:nk))
+    allocate(gs%tm_p_matrix(1:nb, 1:nb, 1:3, 1:nk))
     allocate(gs%rvnl_matrix(1:nb, 1:nb, 1:3, 1:nk))
 
     
@@ -95,7 +97,7 @@ contains
     ! Calculate lattice and reciprocal vectors
     subroutine calc_lattice_info()
         implicit none
-        real(8) :: a12(1:3), a23(1:3), a31(1:3), vol
+        real(8) :: a12(1:3), a23(1:3), a31(1:3), volume
         real(8) :: b1(1:3), b2(1:3), b3(1:3)
 
         a12(1) = a1(2) * a2(3) - a1(3) * a2(2)
@@ -107,10 +109,10 @@ contains
         a31(1) = a3(2) * a1(3) - a3(3) * a1(2)
         a31(2) = a3(3) * a1(1) - a3(1) * a1(3)
         a31(3) = a3(1) * a1(2) - a3(2) * a1(1)
-        vol = dot_product(a12, a3)
-        b1(1:3) = (2d0 * pi / vol) * a23(1:3)
-        b2(1:3) = (2d0 * pi / vol) * a31(1:3)
-        b3(1:3) = (2d0 * pi / vol) * a12(1:3)
+        volume = dot_product(a12, a3)
+        b1(1:3) = (2d0 * pi / volume) * a23(1:3)
+        b2(1:3) = (2d0 * pi / volume) * a31(1:3)
+        b3(1:3) = (2d0 * pi / volume) * a12(1:3)
         
         gs%a_matrix(1:3, 1) = a1(1:3)
         gs%a_matrix(1:3, 2) = a2(1:3)
@@ -118,7 +120,7 @@ contains
         gs%b_matrix(1, 1:3) = b1(1:3)
         gs%b_matrix(2, 1:3) = b2(1:3)
         gs%b_matrix(3, 1:3) = b3(1:3)
-        gs%volume = vol
+        gs%volume = volume
     end subroutine calc_lattice_info
 
 
@@ -126,19 +128,20 @@ contains
     subroutine read_k_data()
         implicit none
         character(256) :: dummy
-        integer :: fh, i, ik, iik
+        integer :: fh, ik, iik
         real(8) :: tmp(4)
-
         fh = open_filehandle(trim(gs_directory) // trim(sysname) // '_k.data', 'old')
-        do i=1, 5
-            read(fh, '(a)') dummy !Skip
-        end do
-        do iik=1, nk
-            read(fh, *) ik, tmp(1:4)
-            if (ik .ne. iik) stop "ERROR! Invalid SYSNAME_k.data"
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)    
+        do ik = 1, nk
+            read(fh, *) iik, tmp(1:4)
+            if (ik .ne. iik) stop "ik mismatch"
             gs%kpoint(1:3, ik) = tmp(1:3)
             gs%kweight(ik) = tmp(4)
-        end do !iik
+        end do
         close(fh)
     end subroutine read_k_data
 
@@ -147,19 +150,20 @@ contains
     subroutine read_eigen_data()
         implicit none
         character(256) :: dummy
-        integer :: fh, i, iik, iib, ib
-        real(8) :: tmp
+        integer :: fh, i, ik, iik, iib, ib
+        real(8) :: tmp(2)
 
         fh = open_filehandle(trim(gs_directory) // trim(sysname) // '_eigen.data', 'old')
-        do i=1, 3
-            read(fh, '(a)') dummy !Skip
-        end do
-        do iik=1, nk
-            read(fh, '(a)') dummy !Skip
-            do iib=1, nb
-                read(fh, *) ib, tmp
-                if (ib .ne. iib) stop "ERROR! Invalid SYSNAME_eigen.data"
-                gs%eigen(ib, iik) = tmp
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        do ik = 1, nk
+            read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+            do ib = 1, nb
+                read(fh, *) iib, tmp(1:2)
+                if (ib .ne. iib) stop "ib mismatch"
+                gs%eigen(ib, ik) = tmp(1)
+                ! gs%occup(ib, ik) = ctmp(2)
             end do
         end do
         close(fh)
@@ -176,35 +180,37 @@ contains
         real(8) :: tmp(1:6)
 
         fh = open_filehandle(trim(gs_directory) // trim(sysname) // '_tm.data', 'old')
-        do i=1, 3
-            read(fh, '(a)') dummy !Skip
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        do ik = 1, nk
+            do ib = 1, nb
+                do jb = 1, nb
+                    read(fh, *) iik, iib, jjb, tmp(1:6)
+                    if (ik .ne. iik) stop "ik mismatch"
+                    if (ib .ne. iib) stop "ib mismatch"
+                    if (jb .ne. jjb) stop "jb mismatch"
+                    gs%tm_p_matrix(ib, jb, 1, ik) = dcmplx(tmp(1), tmp(2))
+                    gs%tm_p_matrix(ib, jb, 2, ik) = dcmplx(tmp(3), tmp(4))
+                    gs%tm_p_matrix(ib, jb, 3, ik) = dcmplx(tmp(5), tmp(6))
+                end do
+            end do
         end do
-        do iik=1, nk
-            do iib=1, nb
-                do jjb=1, nb
-                    read(fh, *) ik, ib, jb, tmp(1:6)
-                    if ((ik .ne. iik) .or. (ib .ne. iib) .or. (jb .ne. jjb)) &
-                        stop "ERROR! Invalid SYSNAME_tm.data"
-                    gs%p_matrix(ib, jb, 1, ik) = dcmplx(tmp(1), tmp(2))
-                    gs%p_matrix(ib, jb, 2, ik) = dcmplx(tmp(3), tmp(4))
-                    gs%p_matrix(ib, jb, 3, ik) = dcmplx(tmp(5), tmp(6))
-                end do !jb
-            end do !ib
-        end do !ik
-        read(fh, '(a)') dummy !Skip
-        do iik=1, nk
-            do iib=1, nb
-                do jjb=1, nb
-                    read(fh, *) ik, ib, jb, tmp(1:6)
-                    if ((ik .ne. iik) .or. (ib .ne. iib) .or. (jb .ne. jjb)) &
-                        stop "ERROR! Invalid SYSNAME_tm.data"
+        read(fh, "(a)") dummy; write(*, "('#>',4x,a)") trim(dummy)
+        do ik = 1, nk
+            do ib = 1, nb
+                do jb = 1, nb
+                    read(fh, *) iik, iib, jjb, tmp(1:6)
+                    if (ik .ne. iik) stop "ik mismatch"
+                    if (ib .ne. iib) stop "ib mismatch"
+                    if (jb .ne. jjb) stop "jb mismatch"
                     gs%rvnl_matrix(ib, jb, 1, ik) = dcmplx(tmp(1), tmp(2))
                     gs%rvnl_matrix(ib, jb, 2, ik) = dcmplx(tmp(3), tmp(4))
                     gs%rvnl_matrix(ib, jb, 3, ik) = dcmplx(tmp(5), tmp(6))
-                end do !jb
-            end do !ib
-        end do !ik
-
+                end do
+            end do
+        end do
+        gs%p_matrix = gs%tm_p_matrix! + gs%rvnl_matrix 
         close(fh)
     end subroutine read_tm_data
 
@@ -212,34 +218,32 @@ contains
     subroutine read_sbe_gs_bin()
         implicit none
         integer :: fh
-
-        fh = get_filehandle()
-        open(fh, file=trim(gs_directory) // trim(sysname) // '_sbe_gs.bin', form='unformatted', status='old')
-        read(fh) gs%kpoint
-        read(fh) gs%kweight
-        read(fh) gs%eigen
-        read(fh) gs%p_matrix
-        read(fh) gs%rvnl_matrix
-        ! read(fh) gs%prod_dk
-        close(fh)
-        return
+        ! fh = get_filehandle()
+        ! open(fh, file=trim(gs_directory) // trim(sysname) // '_sbe_gs.bin', form='unformatted', status='old')
+        ! read(fh) gs%kpoint
+        ! read(fh) gs%kweight
+        ! read(fh) gs%eigen
+        ! read(fh) gs%p_matrix
+        ! read(fh) gs%rvnl_matrix
+        ! ! read(fh) gs%prod_dk
+        ! close(fh)
+        ! return
     end subroutine read_sbe_gs_bin
 
 
     subroutine save_sbe_gs_bin()
         implicit none
         integer :: fh
-
-        fh = get_filehandle()
-        open(fh, file=trim(gs_directory) // trim(sysname) // '_sbe_gs.bin', form='unformatted', status='replace')
-        write(fh) gs%kpoint
-        write(fh) gs%kweight
-        write(fh) gs%eigen
-        write(fh) gs%p_matrix
-        write(fh) gs%rvnl_matrix
-        ! write(fh) gs%prod_dk
-        close(fh)
-        return
+        ! fh = get_filehandle()
+        ! open(fh, file=trim(gs_directory) // trim(sysname) // '_sbe_gs.bin', form='unformatted', status='replace')
+        ! write(fh) gs%kpoint
+        ! write(fh) gs%kweight
+        ! write(fh) gs%eigen
+        ! write(fh) gs%p_matrix
+        ! write(fh) gs%rvnl_matrix
+        ! ! write(fh) gs%prod_dk
+        ! close(fh)
+        ! return
     end subroutine save_sbe_gs_bin
 
 
@@ -248,7 +252,7 @@ contains
     subroutine prepare_matrix()
         implicit none
         integer :: ik, ib, jb
-        real(8), parameter :: omega_eps = 1d-6
+        real(8), parameter :: omega_eps = 1d-9
         complex(8), parameter :: zi = dcmplx(0d0, 1d0)
         do ik=1, nk
             do ib=1, nb
@@ -259,8 +263,11 @@ contains
                         !     & (zi * gs%p_matrix(ib, jb, 1:3, ik) - gs%rvnl_matrix(ib, jb, 1:3, ik)) &
                         !     & / gs%delta_omega(ib, jb, ik)  
                         gs%d_matrix(ib, jb, 1:3, ik) = &
-                            & (zi * gs%p_matrix(ib, jb, 1:3, ik)) &
+                            & zi * (gs%p_matrix(ib, jb, 1:3, ik) +  gs%rvnl_matrix(ib, jb, 1:3, ik)) &
                             & / gs%delta_omega(ib, jb, ik)  
+                        ! gs%d_matrix(ib, jb, 1:3, ik) = &
+                        !     & zi * (gs%p_matrix(ib, jb, 1:3, ik)) &
+                        !     & / gs%delta_omega(ib, jb, ik)  
                     else
                         gs%d_matrix(ib, jb, 1:3, ik) = 0d0
                     end if
