0f718818e7310151f0194716acd36a58bafc5803
Mitsuharu UEMOTO <uemoto@eedept.kobe-u.ac.jp>
2022-11-17 08:06:19 +0900

    Reduce memory

--- sbe_bloch_solver.f90
+++ sbe_bloch_solver.f90
@@ -100,64 +100,63 @@ subroutine dt_evolve_bloch(sbe, gs, Ac, dt)
     real(8), intent(in) :: Ac(1:3)
     real(8), intent(in) :: dt
     complex(8), parameter :: zi = dcmplx(0d0, 1d0)
-    integer :: nb, nk
+    integer :: nb, nk, ik
 
-    complex(8) :: hrho1(1:sbe%nb, 1:sbe%nb, sbe%ik_min:sbe%ik_max)
-    complex(8) :: hrho2(1:sbe%nb, 1:sbe%nb, sbe%ik_min:sbe%ik_max)
-    complex(8) :: hrho3(1:sbe%nb, 1:sbe%nb, sbe%ik_min:sbe%ik_max)
-    complex(8) :: hrho4(1:sbe%nb, 1:sbe%nb, sbe%ik_min:sbe%ik_max)
+    complex(8) :: hrho1_k(1:sbe%nb, 1:sbe%nb)
+    complex(8) :: hrho2_k(1:sbe%nb, 1:sbe%nb)
+    complex(8) :: hrho3_k(1:sbe%nb, 1:sbe%nb)
+    complex(8) :: hrho4_k(1:sbe%nb, 1:sbe%nb)
 
     nb = sbe%nb
     nk = sbe%nk
 
-    call calc_hrho_bloch(sbe%rho, hrho1)
-    call calc_hrho_bloch(hrho1, hrho2)
-    call calc_hrho_bloch(hrho2, hrho3)
-    call calc_hrho_bloch(hrho3, hrho4)
+    !$omp parallel do default(shared) private(ik, hrho1_k, hrho2_k, hrho3_k, hrho4_k)
+    do ik = sbe%ik_min, sbe%ik_max
+        call calc_hrho_bloch_k(ik, sbe%rho(:, :, ik), hrho1_k)
+        call calc_hrho_bloch_k(ik, hrho1_k, hrho2_k)
+        call calc_hrho_bloch_k(ik, hrho2_k, hrho3_k)
+        call calc_hrho_bloch_k(ik, hrho3_k, hrho4_k)
 
-    sbe%rho = sbe%rho + hrho1 * (- zi * dt)
-    sbe%rho = sbe%rho + hrho2 * (- zi * dt) ** 2 * (1d0 / 2d0)
-    sbe%rho = sbe%rho + hrho3 * (- zi * dt) ** 3 * (1d0 / 6d0)
-    sbe%rho = sbe%rho + hrho4 * (- zi * dt) ** 4 * (1d0 / 24d0)
+        sbe%rho(:, :, ik) = sbe%rho(:, :, ik) + hrho1_k * (- zi * dt)
+        sbe%rho(:, :, ik) = sbe%rho(:, :, ik) + hrho2_k * (- zi * dt) ** 2 * (1d0 / 2d0)
+        sbe%rho(:, :, ik) = sbe%rho(:, :, ik) + hrho3_k * (- zi * dt) ** 3 * (1d0 / 6d0)
+        sbe%rho(:, :, ik) = sbe%rho(:, :, ik) + hrho4_k * (- zi * dt) ** 4 * (1d0 / 24d0)
+    end do
     return
 
 contains
 
 
     !Calculate [H, rho] commutation:
-    subroutine calc_hrho_bloch(rho, hrho)
+    subroutine calc_hrho_bloch_k(ik, rho_k, hrho_k)
         implicit none
-        complex(8), intent(in)     :: rho(nb, nb, sbe%ik_min:sbe%ik_max)
-        complex(8), intent(out)    :: hrho(nb, nb, sbe%ik_min:sbe%ik_max)
-        integer :: ik, idir
-        !$omp parallel do default(shared) private(ik,idir)
-        do ik = sbe%ik_min, sbe%ik_max
-            hrho(1:nb, 1:nb, ik) = gs%delta_omega(1:nb, 1:nb, ik) * rho(1:nb, 1:nb, ik)
-            !hrho = hrho + Ac(t) * (p * rho - rho * p)
-            do idir=1, 3 !1:x, 2:y, 3:z
-                ! hrho(1:nb, 1:nb, ik) = hrho(1:nb, 1:nb, ik) + Ac(idir) * (&
-                ! & + matmul(gs%p_matrix(1:nb, 1:nb, idir, ik), rho(1:nb, 1:nb, ik)) &
-                ! & - matmul(rho(1:nb, 1:nb, ik), gs%p_matrix(1:nb, 1:nb, idir, ik)) &
-                ! & )
+        integer, intent(in) :: ik
+        complex(8), intent(in) :: rho_k(nb, nb)
+        complex(8), intent(out) :: hrho_k(nb, nb)
+        integer :: idir
+        !hrho = hrho + Ac(t) * (p * rho - rho * p)
+        hrho_k(1:nb, 1:nb) = gs%delta_omega(1:nb, 1:nb, ik) * rho_k(1:nb, 1:nb)
+        do idir=1, 3 !1:x, 2:y, 3:z
+            ! hrho(1:nb, 1:nb, ik) = hrho(1:nb, 1:nb, ik) + Ac(idir) * (&
+            ! & + matmul(gs%p_matrix(1:nb, 1:nb, idir, ik), rho(1:nb, 1:nb, ik)) &
+            ! & - matmul(rho(1:nb, 1:nb, ik), gs%p_matrix(1:nb, 1:nb, idir, ik)) &
+            ! & )
 
+            call ZGEMM("N","N", sbe%nb, sbe%nb, sbe%nb, &
+                dcmplx(+Ac(idir), 0d0), &
+                gs%p_matrix(:, :, idir, ik),sbe%nb, &
+                rho_k(:, :), sbe%nb, &
+                dcmplx(1d0, 0d0), hrho_k(:, :),sbe%nb)
 
-                call ZGEMM("N","N", sbe%nb, sbe%nb, sbe%nb, &
-                    dcmplx(+Ac(idir), 0d0), &
-                    gs%p_matrix(:, :, idir, ik),sbe%nb, &
-                    rho(:, :, ik), sbe%nb, &
-                    dcmplx(1d0, 0d0),hrho(:, :, ik),sbe%nb)
-
-                call ZGEMM("N","N", sbe%nb, sbe%nb, sbe%nb, &
-                    dcmplx(-Ac(idir), 0d0), &
-                    rho(:, :, ik), sbe%nb, &
-                    gs%p_matrix(:, :, idir, ik),sbe%nb, &
-                    dcmplx(1d0, 0d0), hrho(:, :, ik), sbe%nb)
+            call ZGEMM("N","N", sbe%nb, sbe%nb, sbe%nb, &
+                dcmplx(-Ac(idir), 0d0), &
+                rho_k(:, :), sbe%nb, &
+                gs%p_matrix(:, :, idir, ik),sbe%nb, &
+                dcmplx(1d0, 0d0), hrho_k(:, :), sbe%nb)
                     
-            end do !idir
-        end do !ik
-        !$omp end parallel do
+        end do !idir
         return
-    end subroutine calc_hrho_bloch
+    end subroutine calc_hrho_bloch_k
 end subroutine
 
 function calc_trace(sbe, gs, nb_max, icomm) result(tr)
